e41afd34d3c70ac6ab79b5da52b3988cfe11d672
vsakkas <vasileios.sakkas96@gmail.com>
2018-09-03 22:41:14 +0300

    Added file reading and argument parsing.

--- tldr/tldr.py
+++ tldr/tldr.py
@@ -1,5 +1,18 @@
+import argparse
+
+
+def tldr(file):
+    with open(file, 'r') as f:
+        text = f.read()
+    text = text.replace('\n', '')
+
+
 def main():
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', help='Text file that will be summarized.')
+    args = parser.parse_args()
+
+    tldr(file=args.file)
 
 
 if __name__ == '__main__':
